d2ea15890edd30126bd66cbb97e5b4f3a0b4a1d7
Mohammad Amin <dadgaramin96@gmail.com>
2025-06-01 13:21:18 +0330

    feat: Added testing for the connection!

--- credentials/TemporalApi.credentials.ts
+++ credentials/TemporalApi.credentials.ts
@@ -1,5 +1,8 @@
 import {
+	ICredentialDataDecryptedObject,
+	ICredentialTestRequest,
 	ICredentialType,
+	IHttpRequestOptions,
 	INodeProperties,
 } from 'n8n-workflow';
 
@@ -13,7 +16,7 @@ export class TemporalApi implements ICredentialType {
 			name: 'address',
 			type: 'string',
 			default: 'localhost:7233',
-			description: 'The address of the Temporal server',
+			description: 'The address of the Temporal server (host:port format)',
 			required: true,
 		},
 		{
@@ -23,6 +26,14 @@ export class TemporalApi implements ICredentialType {
 			default: 'default',
 			description: 'The namespace to use for Temporal operations',
 			required: true,
+		},
+		{
+			displayName: 'Connection Timeout (seconds)',
+			name: 'connectTimeout',
+			type: 'number',
+			default: 10,
+			description: 'Timeout in seconds for connecting to the Temporal server',
+			required: false,
 		},
 		{
 			displayName: 'TLS Certificate Path',
@@ -41,4 +52,29 @@ export class TemporalApi implements ICredentialType {
 			required: false,
 		},
 	];
+
+	async authenticate(
+		credentials: ICredentialDataDecryptedObject,
+		requestOptions: IHttpRequestOptions,
+	): Promise<IHttpRequestOptions> {
+		// This method can be used for HTTP-based authentication if needed
+		return requestOptions;
+	}
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'http://{{$credentials.address}}',
+			url: '/api/v1/namespaces/{{$credentials.namespace}}',
+			method: 'GET',
+		},
+		rules: [
+			{
+				type: 'responseCode',
+				properties: {
+					value: 200,
+					message: 'Connection to Temporal server successful',
+				},
+			},
+		],
+	};
 } 
